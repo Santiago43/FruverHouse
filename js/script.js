25501026a9407f82d0bdd59d8b0e803e3cf59279
BuildTools <santipego0001@gmail.com>
2020-10-26 12:24:50 -0500

    Se modifica script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+var direccion="/cgi-bin/FruverHouseBack";
 /**
  * 
- * @param {*} valor 
+ * @param {string} valor 
  */
 function validarEmail(valor) {
     emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}.){1,125}[A-Z]{2,63}$/i;
