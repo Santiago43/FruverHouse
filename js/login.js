7c9fc43b25b6819e0dc0709c88772e1840edbcb5
BuildTools <santipego0001@gmail.com>
2020-10-26 19:51:27 -0500

    Se modifican login y página principal

--- js/login.js
+++ js/login.js
@@ -2,9 +2,13 @@ $("#Ingresar").click(function(){
     var email=$("#email").val();
     var pass=$("#pass").val();
     if(validarEmail(email)){
-
+        var obj={
+            email:email,
+            contra:pass
+        };
+        loginUsuario(obj)
     }else{
-        
+        alert("Formato de correo inválido") 
     }
 
 });
