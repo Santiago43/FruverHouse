6162a723f9aeef3bf2e14ac5a9bb9be434b9d102
BuildTools <santipego0001@gmail.com>
2020-11-02 22:29:53 -0500

    Se modifica cargarselect

--- js/addproducto.js
+++ js/addproducto.js
@@ -2,27 +2,3 @@ $(document).ready(function(){
     cargarSelectCategorias();
 });
 
-function cargarSelectCategorias(){
-    $.ajax({
-        method: 'GET',
-        url: direccion+'/categoria.py',
-        dataType: "json",
-        success: function(response) {
-            cargarSelect(response);
-        },
-        error: function(response){
-            console.log("Error al obtener productos")
-            console.log(JSON.stringify(response))
-        }
-    }); 
-}
-/**
- * Función que carga un select
- * @param {array} categorias 
- */
-function cargarSelect(categorias){
-    for (var i=0;i< categorias.length;i++) {
-        txt='<option value="'+categorias[i].idCategoria+'">'+categorias[i].nombre+'</option>'
-        $("#idCategoria").append(txt);
-    }
-}
--- js/script.js
+++ js/script.js
@@ -292,3 +292,27 @@ function pintarProductos(productos){
     }
 }
 
+function cargarSelectCategorias(){
+    $.ajax({
+        method: 'GET',
+        url: direccion+'/categoria.py',
+        dataType: "json",
+        success: function(response) {
+            cargarSelect(response);
+        },
+        error: function(response){
+            console.log("Error al obtener productos")
+            console.log(JSON.stringify(response))
+        }
+    }); 
+}
+/**
+ * Función que carga un select
+ * @param {array} categorias 
+ */
+function cargarSelect(categorias){
+    for (var i=0;i< categorias.length;i++) {
+        txt='<option value="'+categorias[i].idCategoria+'">'+categorias[i].nombre+'</option>'
+        $("#idCategoria").append(txt);
+    }
+}
