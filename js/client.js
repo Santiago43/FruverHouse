7dc65bbaf90764e774eeb5812b358bab29812ad8
BuildTools <santipego0001@gmail.com>
2020-11-16 18:38:27 -0500

    Se agrega carrito funcionando

--- js/client.js
+++ js/client.js
@@ -36,6 +36,6 @@ function limpiarVentana(){
 
 $("#productos").on("click", ".ui.green.bottom.attached.button", function(){
     var idProducto = $(this).prop("id");
-    setCookie(idProducto,getCookie(idProducto)+1,"3");
+    setCookie(idProducto,parseFloat(getCookie(idProducto))+1,"3");
     alert("Producto añadido al carrito");
 });
