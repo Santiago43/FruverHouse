e287f06f6aa0c41a4dfad553870788d1700cbc9b
BuildTools <santipego0001@gmail.com>
2020-11-16 11:42:21 -0500

    Se agregan productos

---
+++ js/client.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    cargarMenuLateralCategorias();
+});
+
+function cargarMenuLateralCategorias(){
+    $.ajax({
+        method: 'GET',
+        url: direccion+'/categoria.py',
+        dataType: "json",
+        success: function(response) {
+            console.log(response);
+            pintarCategoriasEnMenu(response);
+        },
+        error: function(response){
+            console.log(JSON.stringify(response))
+        }
+    });
+}
+
+function pintarCategoriasEnMenu(categorias){
+    for (let i = 0; i < categorias.length; i++) {
+        $("#categorias") = ' <a class="item"> '+categorias[i].nombre+' </a>';
+    }
+}
+
+$("#categorias").on("click", "a", function(){
+    var idCategory = $(this).prop("id");
+    var obj={idCategoria:idCategory};
+    limpiarVentana();
+    obtenerIdProducto(obj);
+});
+
+function limpiarVentana(){
+    $("#productos").empty();
+}
