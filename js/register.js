36d9d4e8fa62b1d37fc3f6b7eb173684947faa11
Nicolas <luis.arboleda@correo.usa.edu.co>
2020-12-01 18:11:00 -0500

    se agregan registrar de domi y admin

--- js/register.js
+++ js/register.js
@@ -47,4 +47,104 @@ $("#btnRegistrar").click(function(e){
     
 });
 
+$("#btnRegistrarAdmin").click(function(e){
+    e.preventDefault();
+    var primerNombre=$("#n1").val();
+    var segundoNombre=$("#n2").val();
+    var primerApellido=$("#a1").val();
+    var segundoApellido=$("#a2").val();
+    var email=$("#email").val();
+    var emailc=$("#emailc").val();
+    var password=$("#pass").val();
+    var passwordC=$("#passc").val();
+    var telefono=$("#telefono").val();
+    var direccion=$("#direccion").val();
+    var cedula=$("#cedula").val();
+    var tipoDocumento=$("#tipoDocumento").val();
+    var permisos=$("#permisos").val();
+    if (validarEmail(email)){
+        if(email!==emailc){
+            alert("Los correos no coinciden");
+        }
+        else {
+            if (password!==passwordC){
+                alert("Las contraseñas no coinciden");
+            }else{
+                if (!check){
+                    alert("Debe aceptar las políticas de tratamiento de datos");
+                }else{
+                    var obj={
+                        n1: primerNombre,
+                        n2: segundoNombre,
+                        a1: primerApellido,
+                        a2: segundoApellido,
+                        email: email,
+                        contra: password,
+                        telefono: telefono,
+                        direccion: direccion,
+                        cedula: cedula,
+                        tipoDocumento: tipoDocumento,
+                        permisos: permisos
+                    };
+                    registrarAdmin(obj);
+                }
+            }
+        } 
+    }else{
+        alert("Correo en formato incorrecto");
+    }
+    
+});
+
+$("#btnRegistrarDomi").click(function(e){
+    e.preventDefault();
+    var primerNombre=$("#n1").val();
+    var segundoNombre=$("#n2").val();
+    var primerApellido=$("#a1").val();
+    var segundoApellido=$("#a2").val();
+    var email=$("#email").val();
+    var emailc=$("#emailc").val();
+    var password=$("#pass").val();
+    var passwordC=$("#passc").val();
+    var telefono=$("#telefono").val();
+    var direccion=$("#direccion").val();
+    var cedula=$("#cedula").val();
+    var tipoDocumento=$("#tipoDocumento").val();
+    var idTienda=$("#idtienda").val();
+    if (validarEmail(email)){
+        if(email!==emailc){
+            alert("Los correos no coinciden");
+        }
+        else {
+            if (password!==passwordC){
+                alert("Las contraseñas no coinciden");
+            }else{
+                if (!check){
+                    alert("Debe aceptar las políticas de tratamiento de datos");
+                }else{
+                    var obj={
+                        n1: primerNombre,
+                        n2: segundoNombre,
+                        a1: primerApellido,
+                        a2: segundoApellido,
+                        email: email,
+                        contra: password,
+                        telefono: telefono,
+                        direccion: direccion,
+                        cedula: cedula,
+                        tipoDocumento: tipoDocumento,
+                        permisos: permisos,
+                        idTienda: idTienda
+                    };
+                    registrarDomi(obj);
+                }
+            }
+        } 
+    }else{
+        alert("Correo en formato incorrecto");
+    }
+    
+});
+
+
  
