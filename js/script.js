a7e2eed6453068a8d234b24fc372fec3d057a4cb
BuildTools <santipego0001@gmail.com>
2020-12-01 19:38:32 -0500

    Corrección en variable de dirección

--- js/script.js
+++ js/script.js
@@ -391,7 +391,7 @@ var app = new Vue({
             let productoEnCompra= {idProducto:this.productos[i].idProducto,cantidad:this.productos[i].cantidad};
             productosACompra.push(productoEnCompra);
         }
-        var direccionDestino = document.getElementById("#direccionDestino")
+        var direccionDestino = document.getElementById("direccionDestino")
         var txtDireccion = direccionDestino.innerHTML;
         let user = getCookie("usuario")
         let payload ={"data":productosACompra,"user":user,"direccion":txtDireccion};
